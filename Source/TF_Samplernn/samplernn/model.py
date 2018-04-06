5135865622bc5399c9f6f4cb2ee64af3feab2c74
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2018-04-06 15:02:08 +0200

    Further factoring code and adding the possibility to choose an autoregressive order different from FRAME_SIZE

--- Source/TF_Samplernn/samplernn/model.py
+++ Source/TF_Samplernn/samplernn/model.py
@@ -5,7 +5,7 @@ from .ops import mu_law_encode
 
 class SampleRnnModel(object):
   def __init__(self, batch_size, big_frame_size, frame_size,
-             q_levels, rnn_type, dim, n_rnn, seq_len, emb_size):
+             q_levels, rnn_type, dim, n_rnn, seq_len, emb_size, autoregressive_order):
     self.batch_size = batch_size
     self.big_frame_size = big_frame_size
     self.frame_size = frame_size
@@ -15,6 +15,7 @@ class SampleRnnModel(object):
     self.n_rnn = n_rnn
     self.seq_len=seq_len
     self.emb_size=emb_size
+    self.autoregressive_order=autoregressive_order
 
     def single_cell():
       return tf.contrib.rnn.GRUCell(self.dim)
@@ -55,7 +56,7 @@ class SampleRnnModel(object):
     input_frames *= 2.0
     return input_frames
 
-  def _upsampling_reshape(list_frames, upsampling_ratio):
+  def _upsampling_reshape(self, list_frames, upsampling_ratio):
     """Take as input a list of upsampled tensor and reshape them to fit witht the frame_size of the next level
     """
     x = tf.stack(list_frames)
@@ -66,6 +67,7 @@ class SampleRnnModel(object):
      [x_shape[0],
       x_shape[1] * upsampling_ratio,
      -1])
+    return x
 
   def _create_network_BigFrame(self,
     		num_steps,
@@ -80,7 +82,6 @@ class SampleRnnModel(object):
       big_input_frames = self._preprocess_audio_inputs(big_input_frames)
 
       # Note : self.big_frame_size/self.frame_size est le ratio d'upsampling
-    
       big_frame_outputs = []
       big_frame_proj_weights = tf.get_variable(
         "big_frame_proj_weights", [self.dim, self.dim * self.big_frame_size/self.frame_size], dtype=tf.float32)
@@ -89,16 +90,14 @@ class SampleRnnModel(object):
           if time_step > 0: tf.get_variable_scope().reuse_variables()
           (big_frame_cell_output, big_frame_state) = self.big_cell(big_input_frames[:, time_step, :], big_frame_state)
 
-          ##########
-          ##########
+          #>
           # Ici ils utilisent une projection differente pour chaque time_step upsamplee (ce qui est fait dans l'article, pour j dans [0, r-1] representant l'upsampling : c_{(t-1)*r+j} = W_j h_t )
           # Perhaps we can just repeat the prediction self.big_frame_size/self.frame_size times. That would speed things up a little bit
           big_frame_outputs.append(math_ops.matmul(big_frame_cell_output, big_frame_proj_weights))
-          ##########
-          ##########
+          #>
 
         final_big_frame_state = big_frame_state
-      big_frame_outputs = _upsampling_reshape(big_frame_outputs, self.big_frame_size/self.frame_size)
+      big_frame_outputs = self._upsampling_reshape(big_frame_outputs, self.big_frame_size/self.frame_size)
       return big_frame_outputs,final_big_frame_state
 
   def _create_network_Frame(self,
@@ -131,8 +130,7 @@ class SampleRnnModel(object):
 
           frame_outputs.append(math_ops.matmul(frame_cell_output, frame_proj_weights))
       final_frame_state = frame_state
-      import pdb; pdb.set_trace()
-      frame_outputs = _upsampling_reshape(frame_outputs, self.frame_size)   # Actually self.frame_size / 1 as the upsampling ratio of the last level is 1
+      frame_outputs = self._upsampling_reshape(frame_outputs, self.frame_size)   # Actually self.frame_size / 1 as the upsampling ratio of the last level is 1
       return frame_outputs, final_frame_state
 
   def _create_network_Sample(self,
@@ -144,36 +142,46 @@ class SampleRnnModel(object):
       	     sample_input_sequences_shape[1]*self.emb_size,
       	     1]
       embedding = tf.get_variable("embedding", [self.q_levels, self.emb_size])
-      sample_input_sequences = embedding_ops.embedding_lookup(
-                                 embedding, tf.reshape(sample_input_sequences,[-1]))
-      sample_input_sequences = tf.reshape(sample_input_sequences,sample_shap)
+      sample_input_sequences = embedding_ops.embedding_lookup(embedding, tf.reshape(sample_input_sequences,[-1]))
+      # Ici les embedding de chaque frames sont mises les uns a cote des autres (a l'interieur d'un meme batch)
+      sample_input_sequences = tf.reshape(sample_input_sequences, sample_shap)
      
       '''Create a convolution filter variable with the specified name and shape,
       and initialize it using Xavier initialition.'''
       filter_initializer = tf.contrib.layers.xavier_initializer_conv2d()
-      sample_filter_shape = [self.emb_size*2, 1, self.dim]
+      # conv_shape : (kernel_size, channel, output_dim)
+      sample_filter_shape = [self.emb_size*self.autoregressive_order, 1, self.dim]  # self.emb_size*self.autoregressive_order : le noyau de convolution couvre un horizon de deux samples. 
+      # Cf l'argument stride dans la fonction conv1D plus bas : on fait des pas de taille emb_size a chaque fois
       sample_filter = tf.get_variable("sample_filter", sample_filter_shape,
     		initializer = filter_initializer)
-      out = tf.nn.conv1d(sample_input_sequences, 
-      		sample_filter, 
-      		stride=self.emb_size, 
+      out = tf.nn.conv1d(sample_input_sequences,
+      		sample_filter,
+      		stride=self.emb_size,
       		padding="VALID", 
       		name="sample_conv")
       out = out + frame_outputs
+      
+      #> Use self.dim also for MLP ?
       sample_mlp1_weights = tf.get_variable(
         "sample_mlp1", [self.dim, self.dim], dtype=tf.float32)
       sample_mlp2_weights = tf.get_variable(
         "sample_mlp2", [self.dim, self.dim], dtype=tf.float32)
       sample_mlp3_weights = tf.get_variable(
         "sample_mlp3", [self.dim, self.q_levels], dtype=tf.float32)
+      #>
+
+      #>
+      # Pas de biases ?
       out = tf.reshape(out, [-1,self.dim])
       out = math_ops.matmul(out, sample_mlp1_weights)
       out = tf.nn.relu(out)
       out = math_ops.matmul(out, sample_mlp2_weights)
       out = tf.nn.relu(out)
       out = math_ops.matmul(out, sample_mlp3_weights)
-      out = tf.reshape(out, [-1, sample_shap[1]/self.emb_size - 1, self.q_levels])
+      out = tf.reshape(out, [-1, sample_input_sequences_shape[1]-self.autoregressive_order+1, self.q_levels])
+      #>
       return out
+
   def _create_network_SampleRnn(self,
     		train_big_frame_state,
     		train_frame_state):
@@ -198,9 +206,9 @@ class SampleRnnModel(object):
       				input_sequences = input_sequences)
       #sample
       sample_input_sequences = self.encoded_input_rnn[:, 
-                               self.big_frame_size-self.frame_size:-1, :]
+                               self.big_frame_size-self.autoregressive_order:-1, :]
       sample_output = self._create_network_Sample(frame_outputs,
-      			sample_input_sequences = sample_input_sequences)
+      			sample_input_sequences=sample_input_sequences)
       return sample_output, final_big_frame_state, final_frame_state
   def loss_SampleRnn(self,
     	             train_input_batch_rnn,
@@ -211,7 +219,6 @@ class SampleRnnModel(object):
     with tf.name_scope(name):
       # Process input
       self.encoded_input_rnn = mu_law_encode(train_input_batch_rnn, self.q_levels)
-      encoded_rnn = self._one_hot(self.encoded_input_rnn)
         
       # Train
       raw_output, final_big_frame_state, final_frame_state = \
@@ -219,17 +226,15 @@ class SampleRnnModel(object):
 
       with tf.name_scope('loss'):
         # Target
-        target_output_rnn = encoded_rnn[:,self.big_frame_size:,:]
-        target_output_rnn = tf.reshape(target_output_rnn, [-1, self.q_levels])
-        target_output_rnn_decoded = self._decode_one_hot(target_output_rnn)
-        
+        target = tf.reshape(self.encoded_input_rnn[:, self.big_frame_size:], [-1])
+
         # Prediction
         prediction = tf.reshape(raw_output, [-1, self.q_levels])
 
         # Loss
         loss = tf.nn.sparse_softmax_cross_entropy_with_logits( 
             logits=prediction, 
-            labels=target_output_rnn_decoded)
+            labels=target)
         reduced_loss = tf.reduce_mean(loss)
         tf.summary.scalar('loss', reduced_loss)
         if l2_regularization_strength is None:
